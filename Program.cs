bc8b79d3c2088669fd7a64d74e397ee23cba59c3
Jhennifer <jhennifersmithsoares@gmail.com>
2025-03-16 00:32:54 -0300

    organizando código

--- Program.cs
+++ Program.cs
@@ -4,10 +4,20 @@ class Program
 {
     static void Main()
     {
+        //** Lógica de Programação **//
         // ParImpar.ParOuImpar();
         // Tabuada.Tabuadaa();
         // Soma5Numeros.Soma();
-        //  MaiorMenor.MaiorEMenor();
-        Fatorial.Fatoriall();
+        // MaiorMenor.MaiorEMenor();
+        // Fatorial.Fatoriall();
+
+        //** Estrutura de Dados **//
+        // Pilha.Pilhaa();
+        // Fila.Filaa();
+        // Lista.Listaa();
+        // ListaEncadeada.ListaEncadeadaa();
+        // ArvoreBinaria.ArvoreBinariaa();
+        // ArvoreBinariaDeBusca.ArvoreBinariaDeBuscaa();
+        
     }
 }
